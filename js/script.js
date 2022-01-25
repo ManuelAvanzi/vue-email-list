933cefe0fc62873f3610f18d2137c0576bbae94a
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-01-25 23:51:28 +0100

    bonus 1.0

--- js/script.js
+++ js/script.js
@@ -3,20 +3,33 @@ new Vue ({
     el:"#app",
     data:{
         list:[],
+        list2:[],
+        
         mail:'',
+        bonus:'false',
     },
+
     methods:{
 
+        newList:function(){
+            for(let i=0;i<10;i++){
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result)=>{
+            
+            this.list2.push(result.data.response);
+           })
+         }
+        }
     },
 
     mounted() {
       
         for(let i=0;i<10;i++){
 
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result)=>{
+           axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result)=>{
            
            this.list.push(result.data.response);
-        })
-    }
+          })    
+      }
     }
 })
