a0958583ac1d2e0ce712035f0fca0800a272ce19
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-01-25 22:01:55 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,19 @@
+    /*Descrizione:
+    Attraverso l’apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail 
+    generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+    
+    Bonus:
+    Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
+new Vue ({
+    el:"#app",
+    data:{
+        string:'hello boolean',
+    },
+    methods:{
+
+    },
+
+    mounted() {
+      alert(this.string);
+    }
+})
